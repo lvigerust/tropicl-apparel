54c1e9a9fd16aee249dbbf73e286cabad9af427d
lvigerust <l.vigerust@gmail.com>
2022-06-16 09:12:50 +0200

    Moved font import from Head to stylesheet

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,6 @@ const Layout = ({ children }) => {
   return (
     <div className='layout'>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/> 
         <title>Tropicl Apparel</title>
       </Head>
       <header>
@@ -17,7 +16,7 @@ const Layout = ({ children }) => {
         {children}
       </main>
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </div>
   )
