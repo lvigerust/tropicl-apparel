786c0af85fbd1643e0ddc4cff0759193f015fb46
lvigerust <l.vigerust@gmail.com>
2022-06-20 04:13:08 +0200

    Fixed styles for mobile view.

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -8,7 +8,7 @@ import heroBannerStyle from '../styles/HeroBanner.module.css';
 const HeroBanner = ({ heroBanner }) => {
   return (
     <div className={heroBannerStyle.container}>
-      <div>
+      <div className={heroBannerStyle.content}>
         <h1>{heroBanner.largeText1}</h1>
         <h3>{heroBanner.midText}</h3>
         <p className={heroBannerStyle.seasonInfo}>{heroBanner.smallText}</p>
